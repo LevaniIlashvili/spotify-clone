07109f4cfc62b3f26dc6b4e7254855c163fd6f6c
LevaniIlashvili <levaniko900@gmail.com>
2023-10-25 21:13:13 +0400

    add env var for deployment

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,8 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 const clientId = "58896f70fcc34df8abeaf5bbd54e83ba";
-const redirectUri = "http://localhost:5173/";
+const redirectUri =
+  import.meta.env.VITE_REDIRECT_URI || "http://localhost:5173/";
+
 const scope =
   "playlist-modify-public playlist-modify-private user-read-recently-played user-follow-modify user-follow-read user-read-recently-played user-library-modify user-read-private user-read-recently-played  user-top-read playlist-read-private user-library-read";
 
